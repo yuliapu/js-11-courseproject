4881d6921369d412483fa328a19189ee8e83a9a4
Yuliia Putova MTANA <yuliia.putova@mt.com>
2024-03-10 18:22:54 +0100

    getWeekdaysCount

--- scriptTab1.js
+++ scriptTab1.js
@@ -22,6 +22,41 @@ function getDifferenceInMilliseconds(startDate, endDate){
 }
 
 function getDifference(startDate, endDate, unit){
+    endDate = endDate.setDate(endDate.getDate() + 1);
     let timespanInMilliseconds = getDifferenceInMilliseconds(startDate, endDate);
     return convertDuration(timespanInMilliseconds, unit);
 }
+
+function getWeekdaysCount(startDate, endDate){
+   let startDayOfWeek = startDate.getDay();
+   let endDayOfWeek = endDate.getDay();
+   let weekdaysCount = 0;
+   let daysFromStartDateToMonday = 0;
+   let daysFromEndDateToSunday = 0;
+   let totalDays = getDifference(startDate, endDate, "days");
+
+   if (startDayOfWeek !== 1){
+    daysFromStartDateToMonday = 7 - startDayOfWeek + 1;
+    if (startDayOfWeek == 0){
+     weekdaysCount++;
+     daysFromStartDateToMonday = 1;
+    }
+    else {
+     weekdaysCount += 2;
+    }
+   }
+  
+   if (endDayOfWeek !== 0){
+    daysFromEndDateToSunday = endDayOfWeek;
+   }
+
+   totalDays = totalDays - daysFromStartDateToMonday - daysFromEndDateToSunday;
+   weekdaysCount += totalDays / 7 * 2;
+
+   return weekdaysCount;
+}
+
+
+console.log(getWeekdaysCount(new Date("March 10, 2024"), new Date("March 17, 2024")));
+console.log(getWeekdaysCount(new Date("March 1, 2024"), new Date("March 31, 2024")));
+console.log(getWeekdaysCount(new Date("May 1, 2024"), new Date("May 31, 2024")));
